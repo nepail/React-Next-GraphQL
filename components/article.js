cf8690c21cd781ad3a97c57d82f40427832f7457
Jim <jim@phoenix-5.com>
2023-01-10 11:25:56 +0800

    update marquee

--- components/article.js
+++ components/article.js
@@ -2,9 +2,12 @@ export default function Article() {
   return (
     <div className="blog-part right-blog">
       <marquee width="100%" direction="left">
-        <span>Now And Then You Miss It Sounds Make You Cry</span>
+        {/* <span>Now And Then You Miss It Sounds Make You Cry</span>
         <span>Now In - MoMa Sharing Exhibition NOW</span>
-        <span>NYC Opens After Long Lockdown Check</span>
+        <span>NYC Opens After Long Lockdown Check</span> */}
+        <span>記者實地採訪南區當地居民有人大小便不洗手，不知道是真的還是假的</span>
+        <span>南區公園晚上驚傳有一條黃色的狗大聲嚷嚷，原來是被貓嚇到</span>
+        <span>在台中南區，每 60 秒就有一分鐘過去</span>
       </marquee>
       <div className="blog-right-title-container">
         <div className="blog-right-title">Featured Articles</div>
--- components/pinned.js
+++ components/pinned.js
@@ -2,7 +2,7 @@ export default function Pinned() {
   return (
     <div className="blog-header blog-is-sticky">
       <div className="blog-article header-article">
-        <div className="blog-big__title">Self</div>
+        <div className="blog-big__title">南區</div>
         <div className="blog-menu rounded small-title">Pinned Issue</div>
       </div>
       <div className="blog-article page-number">NO. 01</div>
