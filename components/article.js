7408e28e6b84d11b5292b0a3328b319f1174c386
nepail <jim@phoenix-5.com>
2023-01-10 11:37:29 +0800

    fix

--- components/article.js
+++ components/article.js
@@ -8,6 +8,7 @@ export default function Article() {
         <span>記者實地採訪南區當地居民有人大小便不洗手，不知道是真的還是假的</span>
         <span>南區公園晚上驚傳有一條黃色的狗大聲嚷嚷，原來是被貓嚇到</span>
         <span>在台中南區，每 60 秒就有一分鐘過去</span>
+        
       </marquee>
       <div className="blog-right-title-container">
         <div className="blog-right-title">Featured Articles</div>
