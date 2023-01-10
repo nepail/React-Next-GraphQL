6f447ac63a455476c695e09d0a96ac63d03ac0d0
Jim <jim@phoenix-5.com>
2023-01-10 10:11:01 +0800

    fix showChild problem

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,16 @@
+import {useEffect, useState} from 'react'
 import '../styles/globals.css'
 import '../styles/index.scss'
 
 export default function App({ Component, pageProps }) {
+
+  const[showChild, setShowChild] = useState(false)
+  useEffect(()=>{
+    setShowChild(true)
+  }, [])
+
+  if(!showChild){
+    return null
+  }
   return <Component {...pageProps} />
 }
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,36 +1,64 @@
 import { GraphQLClient, gql } from "graphql-request";
 
 const client = new GraphQLClient(
-    "https://api-ap-northeast-1.hygraph.com/v2/clcogbmkd0uq001uh3ypxhqxf/master"
-    )
+  "https://api-ap-northeast-1.hygraph.com/v2/clcogbmkd0uq001uh3ypxhqxf/master"
+);
 
-const IDLIST=gql`
-{
-    posts{
-        id
+const IDLIST = gql`
+  {
+    posts {
+      id
     }
-}
-`
-
-export async function getStaticPaths(){
-    const {posts} = await client.request(IDLIST);
-    return {
-        paths:posts.map((post)=>({params:{id:post.id}})),
-        fallback: false
-    }
-}
-
-export function getStaticProps(){
-    return {
-        props:{
-            post:{title:"測試測試"}
+  }
+`;
+const QUERY = gql`
+  query ($id: ID!) {
+    post(where: { id: $id }) {
+      id
+      title
+      datePublished
+      author {
+        name
+        avatar {
+          url
         }
+      }
+      coverimg {
+        url
+      }
+      content {
+        html
+      }
     }
+  }
+`;
+export async function getStaticPaths() {
+  const { posts } = await client.request(IDLIST);
+  return {
+    paths: posts.map((post) => ({ params: { id: post.id } })),
+    fallback: false,
+  };
 }
 
+export async function getStaticProps({ params }) {
+  const data = await client.request(QUERY, { id: params.id });
 
-export default function Post({post}){
-    return <div className="blog">blog detail
-    <p>{post.title}</p>
+  return {
+    props: {
+      post: data.post,
+    },
+  };
+}
+
+export default function Post({ post }) {
+  return (
+    <div className="blog">
+      blog detail
+      <p>{post.title}</p>
+      <p>{post.datePublished}</p>
+      <p>By {post.author.name}</p>
+      <img src={post.coverimg.url} alt="" />
+      <p dangerouslySetInnerHTML={{ __html: post.content.html }}></p>
     </div>
+  );
 }
