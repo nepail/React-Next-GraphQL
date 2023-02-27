4ba33dca4edd2838760e01e1245258e67833542b
Jim <66344022+nepail@users.noreply.github.com>
2023-02-27 12:01:19 +0800

    Update marquee

--- components/marquee.js
+++ components/marquee.js
@@ -9,6 +9,7 @@ export default function Marquee() {
       </span>
       <span>南區公園晚上驚傳有一條黃色的狗大聲嚷嚷，原來是被貓嚇到</span>
       <span>在台中南區，每 60 秒就有一分鐘過去</span>
+      <span>小黃狗喵喵叫，喵喵喵</span>
     </marquee>
   );
 }
